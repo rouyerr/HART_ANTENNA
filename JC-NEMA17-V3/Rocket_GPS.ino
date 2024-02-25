aabf7a2dfaa52931181934af446a8f9d67ad07a2
rouyerr <97330427+rouyerr@users.noreply.github.com>
2024-02-25 14:20:49 -0800

    Update Rocket_GPS.ino

--- JC-NEMA17-V3/Rocket_GPS.ino
+++ JC-NEMA17-V3/Rocket_GPS.ino
@@ -1,14 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+}
+
 Point Get_Rocket_GPS(Point rocket_gps) {
   Serial.println("\nGETTING ROCKET GPS");
 
-  // FOR REMY
-
-  // rocket_gps = {rocket.latitude, rocket.longitude, rocket.altitude};
+  if (Serial.available() > 0) {
+    String rocket_gps = Serial.readString(); 
+    
 
   // GET ROCKET GPS DATA AND RETURN
-
+  
   Serial.print("Rocket GPS: ");
   Serial.println(rocket_gps.toString());
   return rocket_gps;
-
+  }
 }
