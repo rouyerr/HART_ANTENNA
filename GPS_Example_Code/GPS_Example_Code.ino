2f78533e0bbbe6c46be6fb4857662a4e0defc516
jamescross449 <jamescross449@gmail.com>
2024-03-02 14:50:36 -0800

    Update GPS_Example_Code.ino

--- GPS_Example_Code/GPS_Example_Code.ino
+++ GPS_Example_Code/GPS_Example_Code.ino
@@ -5,7 +5,7 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int RXPin = 3, TXPin = 4;
+static const int RXPin = 33, TXPin = 32;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
